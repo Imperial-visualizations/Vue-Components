bd4cde957adb9d9cc63e1096017dab9505d2c21b
Robert King <robertkng104@gmail.com>
2020-06-19 09:11:20 +0100

    refresh git ignore, restructure library

---
+++ src/components/Button/Button.vue
@@ -0,0 +1,14 @@
+<template>
+    <button class="impvis-button"><slot>Button</slot></button>
+</template>
+<script>
+export default {
+    name:"iv-button"
+}
+</script>
+<style>
+.impvis-button{
+    color:white;
+    background-color:darkblue;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -1,10 +1,21 @@
-import DummyButton from './components/DummyButton.vue'
-import Vue from "vue";
-const Components = {
-  "iv-dummybutton":DummyButton
-};
-for(let key in Components){
-  Vue.component(key,Components[key]);
-}
+// Import Global styles
+import './main.css';
+// Import Components
+import * as components from './components'
 
-export default Components;
+// Install a plugin to Vue.js
+// Can then be installed simply by using the Vue.use syntax, which is preferred by the Vue docs 
+const ImpVisLibrary={
+  install(Vue,options={}){
+    for(const componentName in components){
+      const component = components[componentName]
+      Vue.component(component.name,component);
+    }
+  }
+}
+export default ImpVisLibrary;
+
+// Allow for the plugin to be used if loaded from a CDN
+if (typeof window !== 'undefined' && window.Vue){
+  window.Vue.use(ImpVisLibrary);
+}
