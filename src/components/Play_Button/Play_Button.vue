8a9cf2612b99dd7fa40a895cf5d8c6bd2d954114
Ben R-W <br2417@ic.ac.uk>
2020-06-23 16:41:51 +0100

    Added unicode play/pause symbols; improved event names

--- src/components/Play_Button/Play_Button.vue
+++ src/components/Play_Button/Play_Button.vue
@@ -1,5 +1,6 @@
 <template>
-    <button class="iv-play-button" :class="dyn_class" @click="changeState">{{text}}</button>
+    <button class="iv-play-button" :class="dyn_class" @click="changeState">
+        <span style="float:left; transform: translateY(-5px)">{{symbol}}</span>{{text}}</button>
 </template>
 
 <script>
@@ -9,6 +10,7 @@ export default {
         return {
             play:false,
             text:"Play",
+            symbol:"⏵",
             dyn_class:"iv-button-play",
         }
     },
@@ -17,12 +19,14 @@ export default {
             this.play = !this.play
             if (this.play) {
                 this.text="Pause"
+                this.symbol="⏸"
                 this.dyn_class="iv-button-pause"
-                this.$emit("playevent")
+                this.$emit("iv-played")
             } else {
                 this.text="Play"
+                this.symbol="⏵"
                 this.dyn_class="iv-button-play"
-                this.$emit("pauseevent")
+                this.$emit("iv-paused")
             }
         }
     }
--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,2 @@
-export {default as Button} from "./Button";
-export {default as Slider} from "./Sliders";
 export {default as Play_Button} from "./Play_Button";
 export {DemoButton,DemoSlider} from "./demo";
