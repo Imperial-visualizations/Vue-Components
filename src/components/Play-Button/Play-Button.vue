068eed781a38dfea08feb4d0d21961797b546dca
Ben R-W <br2417@ic.ac.uk>
2020-06-23 10:50:06 +0100

    created files for play/pause button

---
+++ src/components/Play-Button/Play-Button.vue
@@ -0,0 +1,13 @@
+<template>
+    <button class="impvis-play-button"></button>
+</template>
+
+<script>
+export default {
+    name: "iv-play-button"
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/Play-Button/index.js
@@ -0,0 +1,2 @@
+import Play-Button from "./Play-Button.vue";
+export default Play-Button; 
