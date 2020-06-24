b4c34dc6e690339eaa1c3e82a06998a87a83d1e5
Robert King <robertkng104@gmail.com>
2020-06-24 17:32:09 +0100

    change CSS import style

--- src/main.js
+++ src/main.js
@@ -1,12 +1,12 @@
-// Import Global styles
-import './main.css';
-// Import Components
-import * as components from './components'
+
+import * as components from './components';
+
 
 // Install a plugin to Vue.js
 // Can then be installed simply by using the Vue.use syntax, which is preferred by the Vue docs 
 const ImpVisLibrary={
   install(Vue){
+    require("./main.css");
     for(const componentName in components){
       const component = components[componentName]
       Vue.component(component.name,component);
