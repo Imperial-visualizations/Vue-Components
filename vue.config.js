93f307ce00d6f1c733702e4c5bfa46c54a5c5127
Robert King <robertkng104@gmail.com>
2020-06-24 18:12:50 +0100

    attempt to fix SVG loading

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,13 @@
 module.exports = {
     // TODO: We are bundleing CSS directly into the .js file rn, at some stage we may wish to include as seperate file...
-    css:{extract:false}
+    css:{extract:false},
+    chainWebpack: config => {
+    const svgRule = config.module.rule('svg')
+    svgRule.uses.clear()
+
+    svgRule
+      .test(/\.svg$/)
+      .use('svg-url-loader') // npm install --save-dev svg-url-loader
+      .loader('svg-url-loader')
+  }
 }
