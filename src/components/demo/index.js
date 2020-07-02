37af4a9a005a96c2f486cf9b49ec605466bb3cc5
Robert King <robertkng104@gmail.com>
2020-07-02 12:35:24 +0100

    removed demo buttons

--- src/components/demo/index.js
+++
@@ -1,3 +0,0 @@
-import DemoButton from "./DemoButton.vue";
-import DemoSlider from "./DemoSlider.vue"
-export {DemoButton,DemoSlider}; 
--- src/components/index.js
+++ src/components/index.js
@@ -1,7 +1,12 @@
+// For folders containing a single exported component write:
+// export {default as ComponentName} from './ComponentFolder';
+
 export {default as HoverText} from "./HoverText";
 export {default as DropdownTextBox} from "./DropdownTextBox";
 export {default as DropdownList} from "./DropdownList";
 export {default as ToggleBasic} from "./ToggleBasic";
 export {default as ToggleAdvance} from "./ToggleAdvance";
 export {default as Play_Button} from "./Play_Button";
-export {DemoButton,DemoSlider} from "./demo";
+
+// For folders containg multiple exported components write:
+// export {ComponentA,ComponentB} from './ComponentFolder';
