4630da98a5e6f5559c7c7dd5ba0796150e52b9a0
Mark <mt2716@ic.ac.uk>
2020-07-03 17:40:33 +0100

    Added the option to disable the button

--- src/components/HoverButton/HoverButton.vue
+++ src/components/HoverButton/HoverButton.vue
@@ -1,5 +1,5 @@
 <template>
-    <button class="iv-hoverbutton" :class="on_off" @mouseover="changeState" @mouseleave="changeState" v-on:click="buttonClick">
+    <button class="iv-hoverbutton" @mouseover="changeState" @mouseleave="changeState" v-on:click="buttonClick" :disabled="hoverButtonDisabled">
         {{text}}</button>
 </template>
 
@@ -10,28 +10,33 @@ export default {
         return {
             hovering:false,
             text:"Button",
-            on_off:"mouseoffbutton",
-            depressed:false
         }
     },
     methods:{
         changeState(){
             this.hovering = !this.hovering
             if (this.hovering) {
-              this.text="Button"
-                this.on_off="iv-hoverbutton-on"
                 this.$emit("mouseonbutton", this.hovering)
             } else {
-                this.text="Button"
-                this.on_off="iv-hoverbutton-off"
                 this.$emit("mouseoffbutton", this.hovering)
             }
         },
         buttonClick(){
-          this.depressed = !this.depressed
           this.$emit("buttonclicked")
-        }
+        },
+    },
+    props:{
+      initialMode:{
+        type: Boolean,
+        default: true
+      },
+      hoverButtonDisabled: {
+        type:Boolean,
+        required: false,
+        default: false
+      }
     }
+    
 }
 </script>
 
@@ -49,20 +54,7 @@ export default {
     transition: 2s;
 }
 
-.iv-hoverbutton-off{
-    color:white;
-    background-color:darkblue;
-    border: none;
-    padding: 15px 32px;
-    text-align: center;
-    text-decoration: none;
-    display: inline-block;
-    font-size: 16px;
-    box-shadow: 0 8px 16px 0 rgba(20, 102, 224, 0.8), 0 6px 20px 0 rgba(0,0,0,0.19);
-    transition: 2s;
-}
-
-.iv-hoverbutton-on{
+.iv-hoverbutton:hover{
     color:white;
     background-color:darkblue;
     border: none;
@@ -75,29 +67,31 @@ export default {
     transition: 0.2s;
 }
 
-.iv-hoverbutton span {
-  cursor: crosshair;
-  display: inline-block;
-  position: relative;
-  transition: 0.5s;
-  
+.iv-hoverbutton:disabled{
+    color:white;
+    background-color:#ccc;
+    border: none;
+    padding: 15px 32px;
+    text-align: center;
+    text-decoration: none;
+    display: inline-block;
+    font-size: 16px;
+    box-shadow: 0 8px 16px 0 rgba(209, 18, 18, 0), 0 6px 20px 0 rgba(0,0,0,0);
+    transition: 2s;
 }
 
-.iv-hoverbutton span:after {
-  content: '\00bb';
-  position: absolute;
-  opacity: 0;
-  top: 0;
-  right: -20px;
-  transition: 0.5s;
+
+.iv-hoverbutton:hover:disabled{
+    color:white;
+    background-color:#ccc;
+    border: none;
+    padding: 15px 32px;
+    text-align: center;
+    text-decoration: none;
+    display: inline-block;
+    font-size: 16px;
+    box-shadow: 0 8px 16px 0 rgba(209, 18, 18, 0), 0 6px 20px 0 rgba(0,0,0,0);
+    transition: 2s;
 }
 
-.impvis-hoverbutton:hover span {
-  padding-right: 25px;
-}
-
-.impvis-hoverbutton:hover span:after {
-  opacity: 1;
-  right: 0;
-}
 </style>
