7dd4d249f050839451571bfc13745ce1c1ab0683
Mark <mt2716@ic.ac.uk>
2020-07-06 11:42:09 +0100

    Button code cleaned

--- src/components/demo/DemoSlider.vue
+++ src/components/demo/DemoSlider.vue
@@ -1,37 +1,3 @@
-Skip to content
-Search or jump to…
-
-Pull requests
-Issues
-Marketplace
-Explore
- 
-@krammyt 
-Imperial-visualizations
-/
-Vue-Components
-1
-2
-7
-Code
-Issues
-5
-Pull requests
-4
-Actions
-Projects
-Wiki
-Security
-Insights
-Settings
-Vue-Components/src/components/demo/DemoSlider.vue
-@OneOneFour
-OneOneFour renamed dummy components to demo folder
-Latest commit 30f2313 13 days ago
- History
- 1 contributor
-26 lines (25 sloc)  441 Bytes
-  
 <template>
     <div>
         <input type="text" :placeholder="placeholder" v-model="value">
