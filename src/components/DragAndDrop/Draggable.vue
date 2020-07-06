8d34f4ee0ee5339c32eef88ed23920341e3bfd3e
Darren Lean <darrenlean@hotmail.com>
2020-07-06 16:46:00 +0100

    Customisable draggable element

--- src/components/DragAndDrop/Draggable.vue
+++ src/components/DragAndDrop/Draggable.vue
@@ -1,7 +1,8 @@
 <template>
-    <circle @mousedown="drag($event)" @mouseup="drop()" ref="circle" r="25" fill="blue" 
-    :cx="currentX" :cy="currentY">
-    </circle>
+    <g @mousedown="drag($event)" @mouseup="drop()"
+    :transform="`translate(${currentX}, ${currentY})`">
+    <slot></slot>
+    </g>
 </template>
 
 <script>
@@ -39,7 +40,3 @@ export default {
     }
 }
 </script>
-
-<style>
-
-</style>
