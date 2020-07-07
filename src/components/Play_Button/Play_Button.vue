105ee202719ceaa4004e705db5989f7040e5f698
Robert King <robertkng104@gmail.com>
2020-07-07 11:00:34 +0100

    fixed instance of playbutton not working correctly

--- src/components/Play_Button/Play_Button.vue
+++ src/components/Play_Button/Play_Button.vue
@@ -1,7 +1,9 @@
 <template>
-    <component :is="button" class="iv-play-button" :class="dyn_class" @click="changeState">
+    <button class="iv-play-button" :class="dyn_class" @click="changeState">
+        <img v-if="!play" class="play-icon" src="@/components/Play_Button/assets/play.svg" />
+        <img v-else class="play-icon" src="@/components/Play_Button/assets/pause.svg" />
         {{text}}
-    </component>
+    </button>
 </template>
 
 <script>
@@ -20,18 +22,18 @@ export default {
             if (this.play) {
                 this.text="Pause"
                 this.dyn_class="iv-button-pause"
-                this.$emit("played")
+                this.$emit("iv-played")
             } else {
                 this.text="Play"
                 this.dyn_class="iv-button-play"
-                this.$emit("paused")
+                this.$emit("iv-paused")
             }
         }
     }
 }
 </script>
 
-<style>
+<style lang="scss">
 .iv-play-button{
     background-color: #0F8291;
     color: white;
