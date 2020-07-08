c2c3aa53ac5ebcdb87ca5fe57c17173a9d9d471e
Unknown <XH2416@ic.ac.uk>
2020-07-08 16:15:53 +0100

    Remove sub tabs

--- TabTest/TabTest.vue
+++ TabTest/TabTest.vue
@@ -3,17 +3,6 @@
     <iv-tabs>
         <iv-tab tabName="Tab 1" :selected="true">
             <h1>Display Content 1</h1>
-            <iv-tabs>
-                <iv-tab tabName="Sub Tab 1">
-                    <p>Sub Tab Content 1</p>
-                </iv-tab>
-                <iv-tab tabName="Sub Tab 2">
-                    <p>Sub Tab Content 2</p>
-                </iv-tab>
-                <iv-tab tabName="Sub Tab 3">
-                    <p>Sub Tab Content 3</p>
-                </iv-tab>
-            </iv-tabs>
         </iv-tab>
         <iv-tab tabName="Tab 2">
             <h1>Display Content 2</h1>
