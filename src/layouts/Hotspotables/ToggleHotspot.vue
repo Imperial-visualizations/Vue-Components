0b3d34f7d1427292fa59976a8368cecae6e57e6a
Robert King <robertkng104@gmail.com>
2020-08-04 12:27:19 +0100

    added SCSS import for Dan

--- src/layouts/Hotspotables/ToggleHotspot.vue
+++ src/layouts/Hotspotables/ToggleHotspot.vue
@@ -59,11 +59,16 @@ export default {
 }
 </script>
 <style lang="scss">
+@import "src/globals.scss";
 $curvatureRadius:20px;
+
 .iv-toggle-hotspot{
     position:relative;
     width:100%;
     height:100%;
+    >.iv-hello{
+        width:100px; 
+    }
     &.iv-toggled-left,&.iv-toggled-right{
         width:0;
     }
