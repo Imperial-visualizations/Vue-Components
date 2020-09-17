7c10e9f13a1f6416545e2d07ec281f93a1eab6b5
Robert King <robertkng104@gmail.com>
2020-09-17 14:33:08 +0100

    no longer exporting iv-title-bar

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,6 +1,5 @@
 export {default as Visualisation} from './Visualisation';
 export {default as Pane} from './Pane';
-export {default as TitleBar} from './TitleBar';
 export {default as GuidanceModal} from './Guidance';
 export {GuidanceWrapper,GuidanceHome,GuidanceSystem} from './Guidance';
 export {default as Window} from './Window';
