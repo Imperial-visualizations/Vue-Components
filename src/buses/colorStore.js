5607ff697e4335e7b52e215cf463a70b032197a1
Robert King <robertkng104@gmail.com>
2020-09-21 16:25:26 +0100

    added proper themeing support

--- src/buses/colorStore.js
+++ src/buses/colorStore.js
@@ -1,57 +1,48 @@
-import Vue from 'vue';
-const colorStore = new Vue({
-    data(){
-        return{
-            color_input_list:{//THESE ARE THE BLOOMIN COLOURS
-                "navy": [14,64,111],
-                "blue": [0,139,210],
-                "teal": [17,177,162],
-                "green": [168,204,106],
-                "purple": [98,41,107],
-                "berry": [207,16,63],
-                "orange": [239,120,20],
-                "tangerine": [246,176,15],
-                "yellow": [252,217,22]
-            },
-            color_full_list:{
-                "navy": {},
-                "blue": {},
-                "teal": {},
-                "green": {},
-                "purple": {},
-                "berry": {},
-                "orange": {},
-                "tangerine": {},
-                "yellow": {}
-            },
-            shade_Factor_1:0.5,
-            shade_Factor_2:0.3,
-            shade_Factor_3:0.2,
-            shade_Factor_4:0.1,
-            tint_Factor_1:0.1,
-            tint_Factor_2:0.2,
-            tint_Factor_3:0.3,
-            tint_Factor_4:0.5,
-        };
+
+const colorStore ={
+    color_input_list:{//THESE ARE THE BLOOMIN COLOURS
+        "navy": [14,64,111],
+        "blue": [0,139,210],
+        "teal": [17,177,162],
+        "green": [168,204,106],
+        "purple": [98,41,107],
+        "berry": [207,16,63],
+        "orange": [239,120,20],
+        "tangerine": [246,176,15],
+        "yellow": [252,217,22]
     },
-    methods:{
-        produceColors(){
-            for(let k in this.color_input_list){
-                let color_rgb = this.color_input_list[k];
-                this.color_full_list[k].shade_100="rgb("+(color_rgb[0]*(1-this.shade_Factor_1)).toString()+","+ (color_rgb[1]*(1-this.shade_Factor_1)).toString() + "," + (color_rgb[2]*(1-this.shade_Factor_1)).toString()+ ")";
-                this.color_full_list[k].shade_200="rgb("+(color_rgb[0]*(1-this.shade_Factor_2)).toString()+","+ (color_rgb[1]*(1-this.shade_Factor_2)).toString() + "," + (color_rgb[2]*(1-this.shade_Factor_2)).toString()+ ")";
-                this.color_full_list[k].shade_300="rgb("+(color_rgb[0]*(1-this.shade_Factor_3)).toString()+","+ (color_rgb[1]*(1-this.shade_Factor_3)).toString() + "," + (color_rgb[2]*(1-this.shade_Factor_3)).toString()+ ")";
-                this.color_full_list[k].shade_400="rgb("+(color_rgb[0]*(1-this.shade_Factor_4)).toString()+","+ (color_rgb[1]*(1-this.shade_Factor_4)).toString() + "," + (color_rgb[2]*(1-this.shade_Factor_4)).toString()+ ")";
-                this.color_full_list[k].shade_500="rgb("+(color_rgb[0]).toString()+","+ (color_rgb[1]).toString() + "," + (color_rgb[2]).toString()+ ")";
-                this.color_full_list[k].shade_600="rgb("+(color_rgb[0] + (255 - color_rgb[0])*this.tint_Factor_1).toString()+","+ (color_rgb[1] + (255 - color_rgb[1])*this.tint_Factor_1).toString() + "," + (color_rgb[2] + (255 - color_rgb[2])*this.tint_Factor_1).toString()+ ")";
-                this.color_full_list[k].shade_700="rgb("+(color_rgb[0] + (255 - color_rgb[0])*this.tint_Factor_2).toString()+","+ (color_rgb[1] + (255 - color_rgb[1])*this.tint_Factor_2).toString() + "," + (color_rgb[2] + (255 - color_rgb[2])*this.tint_Factor_2).toString()+ ")";
-                this.color_full_list[k].shade_800="rgb("+(color_rgb[0] + (255 - color_rgb[0])*this.tint_Factor_3).toString()+","+ (color_rgb[1] + (255 - color_rgb[1])*this.tint_Factor_3).toString() + "," + (color_rgb[2] + (255 - color_rgb[2])*this.tint_Factor_3).toString()+ ")";
-                this.color_full_list[k].shade_900="rgb("+(color_rgb[0] + (255 - color_rgb[0])*this.tint_Factor_4).toString()+","+ (color_rgb[1] + (255 - color_rgb[1])*this.tint_Factor_4).toString() + "," + (color_rgb[2] + (255 - color_rgb[2])*this.tint_Factor_4).toString()+ ")";    
-            }
-        },
+    color_full_list:{
+        "navy": {},
+        "blue": {},
+        "teal": {},
+        "green": {},
+        "purple": {},
+        "berry": {},
+        "orange": {},
+        "tangerine": {},
+        "yellow": {}
     },
-    created(){
-        this.produceColors();
+    shade_Factor_1:0.5,
+    shade_Factor_2:0.3,
+    shade_Factor_3:0.2,
+    shade_Factor_4:0.1,
+    tint_Factor_1:0.1,
+    tint_Factor_2:0.2,
+    tint_Factor_3:0.3,
+    tint_Factor_4:0.5,
+    produceColors(){
+        for(let k in this.color_input_list){
+            let color_rgb = this.color_input_list[k];
+            this.color_full_list[k].shade_100="rgb("+(color_rgb[0]*(1-this.shade_Factor_1)).toString()+","+ (color_rgb[1]*(1-this.shade_Factor_1)).toString() + "," + (color_rgb[2]*(1-this.shade_Factor_1)).toString()+ ")";
+            this.color_full_list[k].shade_200="rgb("+(color_rgb[0]*(1-this.shade_Factor_2)).toString()+","+ (color_rgb[1]*(1-this.shade_Factor_2)).toString() + "," + (color_rgb[2]*(1-this.shade_Factor_2)).toString()+ ")";
+            this.color_full_list[k].shade_300="rgb("+(color_rgb[0]*(1-this.shade_Factor_3)).toString()+","+ (color_rgb[1]*(1-this.shade_Factor_3)).toString() + "," + (color_rgb[2]*(1-this.shade_Factor_3)).toString()+ ")";
+            this.color_full_list[k].shade_400="rgb("+(color_rgb[0]*(1-this.shade_Factor_4)).toString()+","+ (color_rgb[1]*(1-this.shade_Factor_4)).toString() + "," + (color_rgb[2]*(1-this.shade_Factor_4)).toString()+ ")";
+            this.color_full_list[k].shade_500="rgb("+(color_rgb[0]).toString()+","+ (color_rgb[1]).toString() + "," + (color_rgb[2]).toString()+ ")";
+            this.color_full_list[k].shade_600="rgb("+(color_rgb[0] + (255 - color_rgb[0])*this.tint_Factor_1).toString()+","+ (color_rgb[1] + (255 - color_rgb[1])*this.tint_Factor_1).toString() + "," + (color_rgb[2] + (255 - color_rgb[2])*this.tint_Factor_1).toString()+ ")";
+            this.color_full_list[k].shade_700="rgb("+(color_rgb[0] + (255 - color_rgb[0])*this.tint_Factor_2).toString()+","+ (color_rgb[1] + (255 - color_rgb[1])*this.tint_Factor_2).toString() + "," + (color_rgb[2] + (255 - color_rgb[2])*this.tint_Factor_2).toString()+ ")";
+            this.color_full_list[k].shade_800="rgb("+(color_rgb[0] + (255 - color_rgb[0])*this.tint_Factor_3).toString()+","+ (color_rgb[1] + (255 - color_rgb[1])*this.tint_Factor_3).toString() + "," + (color_rgb[2] + (255 - color_rgb[2])*this.tint_Factor_3).toString()+ ")";
+            this.color_full_list[k].shade_900="rgb("+(color_rgb[0] + (255 - color_rgb[0])*this.tint_Factor_4).toString()+","+ (color_rgb[1] + (255 - color_rgb[1])*this.tint_Factor_4).toString() + "," + (color_rgb[2] + (255 - color_rgb[2])*this.tint_Factor_4).toString()+ ")";    
+        }
     }
-});
+};
 export default colorStore;
--- src/colors.js
+++ src/colors.js
@@ -1,14 +1,33 @@
-export default{
-    primaryDarkBlue:'#133f6f',
-    primaryImperialBlue:'#133f6f',
-    primaryDarkBlue: '#133f6f',
-    primaryMediumBlue:'#008bd2',
-    secondaryMediumGreen: '#11b1a2',
-    secondaryDarkGreen: '#a8cc6a',
-    magenta:'#62296b',
-    red:'#cf103f',
-    orange:'ef7814',
-    yellow:'#fcd916',
-    black: '#242424',
-    white: '#fafafa'
+class Theme{
+    constructor(baseHex){
+        this.main = baseHex;
+    }
+    generateShade(fraction){
+        if(fraction > 1 || fraction < 0) throw Error("fraction must be between 0 and 1");
+            const colorRGB = Theme.hexToRGB(this.main)
+            return Theme.rgbToHex(...colorRGB.map( (v) => Math.round(fraction * (v))))
+    }
+    generateTint(fraction){
+        if(fraction > 1 || fraction < 0) throw Error("fraction must be between 0 and 1");
+        const colorRGB = Theme.hexToRGB(this.main)
+        return Theme.rgbToHex(...colorRGB.map( (v) => Math.round(v + fraction * (255 - v))))
+    }
+    static hexToRGB(hex){
+        const subhex = hex.substring(1)
+        if(subhex.length < 6) throw Error("Must include hex code starting with #")
+        return [parseInt(subhex.substring(0,2),16),parseInt(subhex.substring(2,4),16),parseInt(subhex.substring(4,6),16)]
+    }
+    static rgbToHex(r,g,b){
+        return `#${r.toString(16).padStart(2,"0")}${g.toString(16).padStart(2,"0")}${b.toString(16).padStart(2,"0")}`
+    }
 }
+Theme.DeepBlue = new Theme('#0b3053')
+Theme.Blue = new Theme('#008bd2')
+Theme.Turqouise = new Theme('#11b1a2')
+Theme.Lime = new Theme('#a8cc6a')
+Theme.Purple = new Theme('#62296b')
+Theme.Red = new Theme('#cf103f')
+Theme.Orange = new Theme('#ef7814')
+Theme.Yellow = new Theme('#fcd916')
+Theme.Gold = new Theme('#f6b00f')
+export default Theme;
